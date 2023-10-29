3c14f855d9f8b082d775b7e628581a4b9b5fb299
VladimirImir <mimimvvv@gmail.com>
2023-10-29 22:13:06 +0200

    JUnit 5. 5. - Запуск тестов. Launcher API.

--- src/test/java/com/dev/junit/service/UserServiceTest.java
+++ src/test/java/com/dev/junit/service/UserServiceTest.java
@@ -8,7 +8,7 @@ import java.util.List;
 import static org.junit.jupiter.api.Assertions.*;
 
 @TestInstance(TestInstance.Lifecycle.PER_CLASS)
-class UserServiceTest {
+public class UserServiceTest {
 
     private UserService userService;
 
