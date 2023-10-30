bc75871dbc85cf5491743ebf84bfeff85156c612
VladimirImir <mimimvvv@gmail.com>
2023-10-30 17:06:40 +0200

    JUnit 5. 6. - Test Driven Development. TDD.

--- src/test/java/com/dev/junit/service/UserServiceTest.java
+++ src/test/java/com/dev/junit/service/UserServiceTest.java
@@ -4,11 +4,15 @@ import com.dev.junit.dto.User;
 import org.junit.jupiter.api.*;
 
 import java.util.List;
+import java.util.Optional;
 
 import static org.junit.jupiter.api.Assertions.*;
 
 @TestInstance(TestInstance.Lifecycle.PER_CLASS)
 public class UserServiceTest {
+
+    private static final User IVAN = User.of(1, "Ivan", "123");
+    private static final User PETR = User.of(2, "Petr", "111");
 
     private UserService userService;
 
@@ -36,11 +40,39 @@ public class UserServiceTest {
     @Test
     void usersSizeIfUserAdded() {
         System.out.println("Test 2: " + this);
-        userService.add(new User());
-        userService.add(new User());
+        userService.add(IVAN);
+        userService.add(PETR);
 
         var users = userService.getAll();
         assertEquals(2, users.size());
+    }
+
+    @Test
+    void loginSuccessIfUserExists() {
+        userService.add(IVAN);
+
+        Optional<User> maybeUser = userService.login(IVAN.getUsername(), IVAN.getPassword());
+
+        assertTrue(maybeUser.isPresent());
+        maybeUser.ifPresent(user -> assertEquals(IVAN, user));
+    }
+
+    @Test
+    void loginFailIfPasswordIsNotCorrect() {
+        userService.add(IVAN);
+
+        var maybeUser = userService.login(IVAN.getUsername(), "dummy");
+
+        assertTrue(maybeUser.isEmpty());
+    }
+
+    @Test
+    void loginFailIfUserDoesNotExist() {
+        userService.add(IVAN);
+
+        var maybeUser = userService.login("dummy", IVAN.getPassword());
+
+        assertTrue(maybeUser.isEmpty());
     }
 
     @AfterEach
